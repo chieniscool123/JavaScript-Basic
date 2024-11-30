20e6e41fc08a2e732b5add7866ee2f57154dec44
Chien <chieniscool123@gmail.com>
2024-11-29 19:45:01 -0800

    basic javasript  code

--- function-basic_hw_odin_project.js
+++ function-basic_hw_odin_project.js
@@ -1,8 +1,8 @@
 /* Problem to solve
 For now, just write each function and test the output with console.log.
 
-    Write a function called add7 that takes one number and returns that number + 7.
-    Write a function called multiply that takes 2 numbers and returns their product.
+   done  Write a function called add7 that takes one number and returns that number + 7.
+   done  Write a function called multiply that takes 2 numbers and returns their product.
     Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
     Write a function called lastLetter that takes a string and returns the very last letter of that string:
         lastLetter("abcd") should return "d"
